1936dcf09a2dbefd9f1ad5aaf6343d37b37041df
john javier idmilao <johnjavieridmilao12@gmail.com>
2021-12-06 23:10:26 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const path = require('path')
 
 const app = express()
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+})
+
 //here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
